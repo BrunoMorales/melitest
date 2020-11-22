2978d28ba36f5dddc0f26fe1ea6861363ae19692
BrunoMorales <bruno.j.morales@hotmail.com>
2020-11-22 20:50:33 -0300

    breadcrumb hydrates from fetched categoryPath. Also displays a link per category fetched.

--- src/components/Breadcrumb/Breadcrumb.jsx
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Breadcrumb.scss'
+import fetchCategoryPath from '../../utils/fetchCategoryPath'
 
-const Breadcrumb = () => {
+const Breadcrumb = ({categoryId}) => {
+    const [breadcrumb, setBreadcrumb] = useState()
+    useEffect(()=> {
+        fetchCategoryPath(categoryId)
+        .then((res)=>setBreadcrumb(res))
+    })
 
-
-    return <div>
-
-    </div>
+    return (
+        <nav>
+            {breadcrumb?.map(
+                (category, index)=>(
+                        <Link key={index} to={`/items?search=${category.name}`}>
+                            {category.name}
+                        </Link>
+                )
+            )}
+        </nav>  
+    )
 }
 
 export default Breadcrumb
