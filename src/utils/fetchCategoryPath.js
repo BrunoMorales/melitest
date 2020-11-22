3ba2f10702a6c0eed8458d6bdfb3eff31333c9be
BrunoMorales <bruno.j.morales@hotmail.com>
2020-11-22 20:39:05 -0300

    Added fetchCategories wireframe

---
+++ src/utils/fetchCategoryPath.js
@@ -0,0 +1,10 @@
+const fetchCategoryPath = async (categoryId) => {
+    try {
+
+
+    } catch (error) {
+        console.log('Error while fetching product category. ', error)
+    }
+}
+
+export default fetchCategoryPath
