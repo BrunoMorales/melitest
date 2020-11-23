d4b0130bc4f9e63ca0f865e40e9756a244e30cda
BrunoMorales <bruno.j.morales@hotmail.com>
2020-11-22 22:37:26 -0300

    Clicking on searchbar logo will clear the searchbox

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -10,13 +10,15 @@ const SearchBar = () => {
     const searchLink = searchInput ? `/items?search=${searchInput}` : '/'
     const handleType = ({ target: { value } }) => setSearchInput(value)
     const submitOnEnter = ({keyCode}) => { if (keyCode === 13) { routerHistory.push(searchLink)} }
+    const clearSearchbox = () => setSearchInput('')
 
     return (
         <div className='header'>
             <div className='search-wrapper'>
                 <Link to='/' >
                     <img alt='meli_logo' className='header-logo'
-                        src='https://http2.mlstatic.com/frontend-assets/ui-navigation/5.6.0/mercadolibre/logo__small.png' />
+                        src='https://http2.mlstatic.com/frontend-assets/ui-navigation/5.6.0/mercadolibre/logo__small.png' onClick={clearSearchbox} 
+                    />
                 </Link>
                 <div className='searchbar'>
                     <input
