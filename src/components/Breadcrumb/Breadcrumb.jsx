e40d49f9cc9124c48ff8243e4c1cf957667a588b
BrunoMorales <bruno.j.morales@hotmail.com>
2020-11-22 22:08:55 -0300

    Styled breadcrumb

--- src/components/Breadcrumb/Breadcrumb.jsx
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 import './Breadcrumb.scss'
 import fetchCategoryPath from '../../utils/fetchCategoryPath'
+import  {FaChevronRight}  from 'react-icons/fa'
 
 const Breadcrumb = ({categoryId}) => {
     const [breadcrumb, setBreadcrumb] = useState()
@@ -11,12 +12,15 @@ const Breadcrumb = ({categoryId}) => {
     })
 
     return (
-        <nav>
+        <nav className='breadcrumb'>
             {breadcrumb?.map(
-                (category, index)=>(
-                        <Link key={index} to={`/items?search=${category.name}`}>
-                            {category.name}
-                        </Link>
+                (category, index)=>( 
+                <Fragment key={index}>
+                    {index > 0 && <FaChevronRight className='category-divider'/>}
+                    <Link to={`/items?search=${category.name}`} className='breadcrumb-link'>
+                        {category.name}
+                    </Link>
+                </Fragment>
                 )
             )}
         </nav>  
