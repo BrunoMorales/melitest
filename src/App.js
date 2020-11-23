40790e73a752e82eeedc9a209aac6c6089127190
BrunoMorales <bruno.j.morales@hotmail.com>
2020-11-22 23:12:00 -0300

    Improved some styles

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import SearchBar from './components/SearchBar';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import ProductList from './pages/ProductList';
 import ProductDetail from './pages/ProductDetail';
+import './App.scss'
 
 function App() {
   return (
     <BrowserRouter>
     <SearchBar />
+    <div  className='App-wrapper'>
+
     <Switch>
       <Route path='/items/:id' component={ProductDetail} />
       <Route path='/items' component={ProductList} />
       <Route path='/' />
     </Switch>
+    </div>
   </BrowserRouter >
   );
 }
